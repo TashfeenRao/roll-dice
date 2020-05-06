01c5223551cafa40c616fb1f21e80f1d97b89de2
tashfeenrao <raotashfeen25@gmail.com>
2020-05-07 02:46:01 +0500

    initialize project

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    watch: true,
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+      rules: [
+          {
+              test: /\.css$/,
+              use: [
+                  'style-loader',
+                  'css-loader'
+              ]
+          }
+      ]
+  }
+};
