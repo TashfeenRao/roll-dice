5a9112f3d061d58051299e5d8d1875a923d2ca9f
tashfeenrao <raotashfeen25@gmail.com>
2020-05-09 01:35:16 +0500

    score greater then 50 will be winner

--- src/dom.js
+++ src/dom.js
@@ -2,6 +2,12 @@ import Initilizer from './initializer';
 
 const dom = (() => {
   const init = new Initilizer();
+  const checkWin = () => {
+    if (init.scores[init.activePlayer] > 50) {
+      document.getElementById(`name-${init.activePlayer}`).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+    }
+  }
   const updateActive = () => {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
@@ -11,7 +17,7 @@ const dom = (() => {
     document.querySelector(`#current-${init.activePlayer}`).textContent = init.roundScore;
     init.activePlayer = init.activePlayer === 0 ? 1 : 0;
     updateActive();
-  }
+  };
   const updateRoundScore = (diceRound) => {
     if (diceRound !== 1) {
       init.roundScore += diceRound;
@@ -30,6 +36,7 @@ const dom = (() => {
   const addScore = () => {
     init.scores[init.activePlayer] += init.roundScore;
     document.getElementById(`score-${init.activePlayer}`).textContent = init.scores[init.activePlayer];
+    checkWin();
     switchPlayer();
   }
   const displayScore = () => {
